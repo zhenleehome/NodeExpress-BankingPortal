9f65b1fa4c059280655e562eaefe577abc6cd892
zhenleehome <46949934+zhenleehome@users.noreply.github.com>
2019-01-24 20:44:12 -0700

    zl: module 5

--- src/app.js
+++ src/app.js
@@ -17,40 +17,16 @@ app.get('/', (req, res) => res.render('index',
     accounts
 }));
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings});
-});
 
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking});
-});
+const accountRoutes = require('./routes/accounts.js');
+const servicesRoutes = require('./routes/services.js');
 
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit});
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]});
 })
 
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-})
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-})
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10);
-
-    writeJSON();
-    res.render('payment', {message: "Payment Successful", account: accounts.credit});
-})
 
 app.listen(3000, () => console.log('PS project running on port 3000!'));
---
+++ src/routes/services.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const { accounts, users, writeJSON } = require('../data');
+
+router.get('/transfer', (req, res) => {
+    res.render('transfer');
+})
+router.post('/transfer', (req, res) => {
+    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+
+    writeJSON();
+    res.render('transfer', {message: 'Transfer Completed'});
+})
+
+router.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+router.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10);
+
+    writeJSON();
+    res.render('payment', {message: "Payment Successful", account: accounts.credit});
+})
+
+module.exports = router;
